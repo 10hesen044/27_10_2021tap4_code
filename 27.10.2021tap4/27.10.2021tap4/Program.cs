d1f814c36542114bec52b7df527f2d58870bc352
10hesen044 <tu201906250@code.edu.az>
2021-10-27 12:09:48 +0400

    project added

---
+++ 27.10.2021tap4/27.10.2021tap4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _27._10._2021tap4
+{
+    class Program
+    {
+        static void Main(string[] args) //TAPSIRIQ 4 27.10.2021
+        {
+            int[] array = { 13, 5, 3, 9, 56, 34, 24 };
+            Console.WriteLine("Arrayda ki max element: "+MaxElement(array));
+        }
+        static int MaxElement(int[] Arr)
+        {
+            int max = Arr[0];
+            foreach (var item in Arr)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+
+    }
+}
